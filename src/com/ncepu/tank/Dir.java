aa482a0905f55d345c796eb95494e9fe751fbea7
Administrator <xgq.ml@foxmail.com>
2020-03-26 22:06:55 +0800

    test

---
+++ src/com/ncepu/tank/Dir.java
@@ -0,0 +1,5 @@
+package com.ncepu.tank;
+
+public enum Dir {
+    L, U, R, D, STOP
+}
---
+++ src/com/ncepu/tank/Main.java
@@ -0,0 +1,19 @@
+package com.ncepu.tank;
+
+public class Main {
+    public static void main(String[] args){
+        TankFrame tf=new TankFrame();
+
+        tf.setVisible(true);
+
+        for (;;){
+            try {
+                Thread.sleep(25);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            tf.repaint();
+        }
+    }
+}
